a06e432518b09161acfb06246c503c7ba35d1d70
Lendrock <lchoxon-2021286@kinal.edu.gt>
2025-08-19 12:08:12 -0600

    Cambios y nuevos archivos al proyecto registro_cliente

--- src/main/java/org/flim/registro_cliente/RegistroClienteApplication.java
+++ src/main/java/org/flim/registro_cliente/RegistroClienteApplication.java
@@ -1,13 +1,57 @@
 package org.flim.registro_cliente;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
+
 @SpringBootApplication
-public class RegistroClienteApplication {
+public class RegistroClienteApplication implements CommandLineRunner {
+
+	//Profesionalizar nuestro SOUT con un logger
+	private static final Logger logger = LoggerFactory.getLogger(RegistroClienteApplication.class);
+	//Agregar un String para el salto de linea
+	String salto = System.lineSeparator();
 
 	public static void main(String[] args) {
+		//antes de iniciar
+		logger.info("Iniciado la aplicacion");
 		SpringApplication.run(RegistroClienteApplication.class, args);
+		//al finalizar
+		logger.info("Aplicacion finalizada");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		registroClientesApp();
+	}
+	private void registroClientesApp(){
+		logger.info("+++++Bienvenido a la aplicacion de Registro de Cliente+++++");
+		var salir = false;
+		var consola = new Scanner(System.in);
+		while (!salir){
+			var opcion = mostrarMenu(consola);
+			salir = ejecutarOpciones(consola, opcion);
+			logger.info(salto);
+		}
+	}
+	private int mostrarMenu(Scanner consola){
+		logger.info("""
+				***Aplicacion***
+				1.Listar Cliente
+				2.Buscar Cliente
+				3.Agregar Cliente
+				4.Mopdificar Cliente
+				5.Eliminar Cliente
+				6.Salir
+				""");
+		return 0;
+	}
+	private boolean ejecutarOpciones(Scanner consola, int opcion){
+		return false;
+	}
 }
